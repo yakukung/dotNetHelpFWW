cd76df15700f52f089b246c7002154c523baaae0
Yakukung <yakukung2003@gmail.com>
2025-03-28 15:40:44 +0700

    first commit

---
+++ MauiApp1/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MauiAppPro.Pages;
+
+namespace MauiAppPro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
+	}
+}
---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using MauiAppPro.ViewsModel; // เพิ่ม namespace
+
+namespace MauiAppPro;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+        // ลงทะเบียน HomeViewModel ใน DI Container
+        builder.Services.AddTransient<HomeViewModel>();
+
+        return builder.Build();
+    }
+}
